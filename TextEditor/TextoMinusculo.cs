8a38ec003a0c46707d10dae448747be9204d01ac
AldebaraFork <eduardoamorim758@proton.me>
2024-05-21 15:42:45 -0300

    Now you can save your file in to lower

--- TextEditor/TextoMinusculo.cs
+++ TextEditor/TextoMinusculo.cs
@@ -11,8 +11,57 @@ namespace TextEditorV2
         public void textoMinusculo()
         {
             Console.WriteLine("Digite o texto que deseja traduzir para minúsculo: ");
-            string TextoInserido = Console.ReadLine();
-            Console.WriteLine("Texto traduzido para minúsculo: " + TextoInserido.ToLower());
+            Console.WriteLine("---------------------------------------------------");
+            string TextoInserido = null;
+            do
+            {
+                TextoInserido = Console.ReadLine().ToLower();
+                TextoInserido += Environment.NewLine;
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.WriteLine($"Texto inserido: {TextoInserido}");
+
+
+            Console.WriteLine("Deseja salvar o arquivo? S/N");
+            char resposta = char.Parse(Console.ReadLine().ToLower());
+            if (resposta == 's')
+            {
+                SalvarTexto salvarTexto = new SalvarTexto();
+                salvarTexto.SalvarTextos(TextoInserido);
+            }
+            else if (resposta == 'n')
+            {
+                Console.WriteLine("deseja retornar ao menu ou sair do aplicativo? MENU/SAIR");
+                string Opcao = Console.ReadLine().ToLower();
+                if (Opcao == "menu")
+                {
+                    Console.WriteLine("Retornando ao menu");
+                    Thread.Sleep(3000);
+                    Menu menu = new Menu();
+                    menu.Menuzada();
+                }
+                else if (Opcao == "sair")
+                {
+                    Console.WriteLine("Saindo do aplicativo");
+                    Thread.Sleep(3000);
+                    System.Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("opção invalida! saindo do aplicativo");
+                    
+                }
+                       
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Opção invalida! retornando ao menu principal");
+                Thread.Sleep(3500);
+                Menu menu = new Menu();
+                menu.Menuzada();
+            }
         }
     }
 }
