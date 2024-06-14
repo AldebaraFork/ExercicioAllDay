0a4d0c2a46045f3e4ff580deb13bd8db8e4f228c
AldebaraFork <eduardoamorim758@proton.me>
2024-06-13 22:04:58 -0300

    POO improvements

--- Calculadorav2/Multiplicar.cs
+++ Calculadorav2/Multiplicar.cs
@@ -15,9 +15,58 @@ namespace CalculadoraV2
             double Numer1 = double.Parse(Console.ReadLine());
             Console.Write("Digite o segundo numero para multiplicar:");
             double Numer2 = double.Parse(Console.ReadLine());
-            double Numer3 = Numer1 * Numer2;
-            Console.WriteLine("A multiplicação de " + Numer1 + " * " + Numer2 + " é: " + Numer3);
+            double Resultado = Numer1 * Numer2;
+            Console.WriteLine("A multiplicação de " + Numer1 + " * " + Numer2 + " é: " + Resultado);
 
+
+        }
+
+
+        public void SalvarArquivo(double Resultado)
+        {
+            Menu menu = new Menu();
+            Console.WriteLine("Deseja salvar arquivo? S/N");
+            var resposta = Console.ReadLine().ToLower();
+            //SALVAR ARQUIVO
+            if (resposta == "s" || resposta == "sim")
+            {
+                Console.Clear();
+                Console.Write("Aonde deseja salvar o arquivo: ");
+                var path = Console.ReadLine();
+                using (var arquivo = new StreamWriter(path))
+                {
+                    arquivo.WriteLine(Resultado);
+                }
+                Console.WriteLine($"Arquivo salvo com sucesso em {path}");
+                Console.WriteLine("Voltando ao menu principal");
+                Thread.Sleep(3000);
+                Console.Clear();
+                menu.Menuzada();
+            }
+            //NAO SALVAR ARQUIVO
+            else if (resposta == "n" || resposta == "nao")
+            {
+                Console.WriteLine("Arquivo não salvo! retornando ao menu principal....");
+                Thread.Sleep(2000);
+                Console.Clear();
+                menu.Menuzada();
+            }
+            // VAZIO
+            else if (string.IsNullOrEmpty(resposta))
+            {
+                Console.WriteLine("Por-favor digite algo! tente novamente");
+                Thread.Sleep(2000);
+                Console.Clear();
+                SalvarArquivo(Resultado);
+            }
+            //RESPOSTA INCORRETA
+            else
+            {
+                Console.WriteLine("Opção invalida! tente novamente");
+                Thread.Sleep(2000);
+                Console.Clear();
+                SalvarArquivo(Resultado);
+            }
         }
     }
 }
--- Calculadorav2/Subtracao.cs
+++ Calculadorav2/Subtracao.cs
@@ -11,14 +11,118 @@ namespace CalculadoraV2
     {
         public void CalcularSubtracao()
         {
+            Menu menu = new Menu();
+
             Console.WriteLine("Você selecionou a opção Subtração! Por-favor utilize virgula para numeros quebrados! ");
             Console.Write("Digite o primeiro numero para subtrair:");
             double Numer1 = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
             Console.Write("Digite o segundo numero para subtrair:");
             double Numer2 = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
-            double Numer3 = Numer1 - Numer2;
-            Console.WriteLine("A subtração de " + Numer1 + " - " + Numer2 + " é: " + Numer3.ToString(CultureInfo.InvariantCulture));
+            double Resultado = Numer1 - Numer2;
+            Console.WriteLine("A subtração de " + Numer1 + " - " + Numer2 + " é: " + Resultado.ToString(CultureInfo.InvariantCulture));
 
+            //SALVA A SUBTRAÇÃO 
+            try
+            {
+                SalvarResultado(Resultado);
+                Thread.Sleep(2000);
+            }
+            catch (Exception exemplo)
+            {
+                Console.WriteLine(exemplo.InnerException);
+                Console.WriteLine(exemplo.Message);
+                Console.WriteLine("Aconteceu um erro inesperado.....");
+            }
+
+            RetornarMenu();
+        }
+        //Salvar Subtração
+        public void SalvarResultado(double Resultado)
+        {
+            Menu menu = new Menu();
+
+            Console.WriteLine("Deseja salvar esse arquivo? S/N");
+            var resposta = Console.ReadLine().ToLower();
+            if (resposta == "s" || resposta == "sim")
+            {
+                Console.Clear();
+                Console.WriteLine("Qual caminho deseja salvar o arquivo? ");
+                var path = Console.ReadLine();
+                using (var arquivo = new StreamWriter(path))
+                {
+                    //salvando arquivo inserido no path
+                    arquivo.WriteLine(Resultado);
+
+                }
+                Console.Clear();
+                Console.WriteLine($"Arquivo salvo com sucesso em {path}");
+                Console.WriteLine("Voltando ao menu principal");
+                Thread.Sleep(3000);
+                Console.Clear();
+
+                menu.Menuzada();
+            }
+            else if (resposta == "nao" || resposta == "n")
+            {
+                Console.Clear();
+                Console.WriteLine("Arquivo não salvo! retornando ao menu....");
+                Thread.Sleep(2000);
+                Console.Clear();
+                menu.Menuzada();
+            }
+            else if (string.IsNullOrEmpty(resposta))
+            {
+                Console.WriteLine("Por-favor insira um valor valido....");
+                Thread.Sleep(2000);
+                Console.Clear();
+                CalcularSubtracao();
+            }
+        }
+
+        public void RetornarMenu()
+        {
+            try
+            {
+                Console.Clear();
+                Menu menu = new Menu();
+                Console.WriteLine("Deseja retornar ao menu ou sair? MENU/SAIR");
+                string RespMenu = Console.ReadLine().ToLower();
+                if (RespMenu == "menu")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Retornando ao menu principal.......");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    menu.Menuzada();
+                }
+                else if (RespMenu == "sair")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Saindo.....");
+                    Thread.Sleep(2000);
+                    System.Environment.Exit(0);
+                }
+                else if (string.IsNullOrEmpty(RespMenu))
+                {
+                    Console.WriteLine("Por-favor insira algum valor...");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    RetornarMenu();
+                }
+                else
+                {
+                    Console.WriteLine("Opção invalida! retornando ao menu principal.....");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    menu.Menuzada();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine("Algo deu errado");
+            }
         }
     }
 }
