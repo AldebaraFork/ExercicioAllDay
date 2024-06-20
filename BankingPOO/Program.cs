265f27f4aac16f83707548712bea4d127eb80426
AldebaraFork <eduardoamorim758@proton.me>
2024-06-19 22:12:44 -0300

    banking system to training POO

---
+++ BankingPOO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlTypes;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BankingPOO
+{
+    internal class Program
+    {
+        //CLASSE PRINCIPAL
+        static void Main(string[] args)
+        {
+            GerenciarDinheiro dindin = new GerenciarDinheiro();
+
+            dindin.Money = double.Parse(Console.ReadLine());
+            Console.WriteLine(dindin.Money);
+            
+
+        }
+
+    }
+
+    //COMO USAR O GET E SET
+    public class GerenciarDinheiro
+    {
+        public double Money { get; set; }
+
+
+
+    }
+}
