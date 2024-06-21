89ac6ec3d8aa51f0ff23de4ce8cbd8c132554dc7
AldebaraFork <eduardoamorim758@proton.me>
2024-06-20 22:20:59 -0300

    Methods and Properties exercises

--- BankingPOO/Program.cs
+++ BankingPOO/Program.cs
@@ -8,22 +8,59 @@ namespace BankingPOO
         //CLASSE PRINCIPAL
         static void Main(string[] args)
         {
-            GerenciarDinheiro dindin = new GerenciarDinheiro();
 
-            dindin.Money = double.Parse(Console.ReadLine());
-            Console.WriteLine(dindin.Money);
-            
+
+
 
         }
 
     }
 
     //COMO USAR O GET E SET
-    public class GerenciarDinheiro
+    public class DadosPagamento()
     {
-        public double Money { get; set; }
+        //PIX
+        public string CodigoPix { get; set; }
 
 
+        //DADOS CARTAO
+        public string NumeroCartao { get; set; }
+        public string NomeCartao { get;  set; }
+        public DateTime ValidadeCartao { get; set; }
 
+        public string CodigoSeguranca { get; set; }
+
+
+    }
+    public class Pagamentos : DadosPagamento
+    {
+        public DateTime Vencimento { get; set; }
+
+        public void PagarComCartao()
+        {
+            Console.Write("Por-favor insira os numeros no cartão: ");
+            NumeroCartao = Console.ReadLine();
+            Console.Write("Agora insira o nome impresso no cartão");
+            NomeCartao = Console.ReadLine().ToUpper();
+            Console.Write("Insira o Codigo de segurança atrás do cartão: ");
+
+
+            //VALIDAÇÃO CVV
+            CodigoSeguranca = Console.ReadLine();
+            if (CodigoSeguranca.Length > 3)
+            {
+                Console.WriteLine("Valor invalido!");
+                Thread.Sleep(2000);
+                Console.Clear();
+                PagarComCartao();
+
+                
+            }else if (CodigoSeguranca.Length <= 3)
+            {
+                Console.WriteLine("Obrigado por informar! ");
+
+            }
+            Console.WriteLine("Dados do cartão salvo ! ");
+        }
     }
 }
