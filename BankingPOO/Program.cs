91467b25fdb9c3c6d90fb058312dad3130a33ecd
AldebaraFork <eduardoamorim758@proton.me>
2024-06-25 20:22:38 -0300

    Now you can save your file, and payment methods is working

--- BankingPOO/Program.cs
+++ BankingPOO/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.SqlTypes;
+﻿using System;
+using System.Data.SqlTypes;
+using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
 
 namespace BankingPOO
@@ -9,7 +11,7 @@ namespace BankingPOO
         static void Main(string[] args)
         {
 
-           DadosUsuario dadosIniciais = new DadosUsuario();
+            DadosUsuario dadosIniciais = new DadosUsuario();
             dadosIniciais.ObterDadosUsuario();
 
         }
@@ -20,8 +22,8 @@ namespace BankingPOO
     public class DadosUsuario()
     {
         //DADOS USUÁRIO
-        public string Nome { get; set; }
-        public string Cpf { get; set; }
+        protected string Nome { get; set; }
+        private string Cpf { get; set; }
         public short Idade { get; set; }
 
         //OBTEM OS DADOS DO USUARIO
@@ -56,7 +58,7 @@ namespace BankingPOO
                 Console.Clear();
                 ObterDadosUsuario();
             }
-         
+
         }
 
 
@@ -67,18 +69,21 @@ namespace BankingPOO
     public class PagamentoCartao : DadosUsuario
     {
         //DADOS CARTAO
-        public string NumeroCartao { get; set; }
-        public string NomeCartao { get; set; }
-        public DateTime ValidadeCartao { get; set; }
+        private string NumeroCartao { get; set; }
+        private string NomeCartao { get; set; }
 
-        public string CodigoSeguranca { get; set; }
 
-        public DateTime Vencimento { get; set; }
+        private string CodigoSeguranca { get; set; }
+
+        public double Valor { get; set; }
 
         //PAGAMENTO CARTAO
 
         public void PagarComCartao()
         {
+            //DADOS CARTÃO
+            SalvarPagamentos salva = new SalvarPagamentos();
+            Menu menu = new Menu();
             Console.Write("Por-favor insira os numeros no cartão: ");
             NumeroCartao = Console.ReadLine();
             Console.Write("Agora insira o nome impresso no cartão: ");
@@ -91,11 +96,43 @@ namespace BankingPOO
             //VALIDAÇÃO CORRETA
             if (CodigoSeguranca.Length == 3 || CodigoSeguranca.Length == 4)
             {
-                Console.WriteLine("Valor invalido!");
-                Thread.Sleep(2000);
+                //VALIDAÇÃO CREDITO OU DEBITO
                 Console.Clear();
-                PagarComCartao();
+                Console.WriteLine("Dados do cartão salvo! Débito ou Crédito? (DEBITO/CREDITO)");
+                var DebitoOuCredito = Console.ReadLine().ToLower();
+                //DEBITO
+                if (DebitoOuCredito == "debito")
+                {
+                    Console.WriteLine("Qual o valor do pagamento?");
+                    Valor = double.Parse(Console.ReadLine().ToString(CultureInfo.CurrentCulture));
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"Pagamento realizado no valor de R$:{Valor} na função DEBITO");
+                    Thread.Sleep(4000);
+                    Console.Clear();
 
+                    //SALVAR ARQUIVO DE PAGAMENTO
+                    salva.SalvarValor(Valor);
+                }
+                //CREDITO
+                else if (DebitoOuCredito == "credito")
+                {
+                    Console.WriteLine("Qual o valor do pagamento?");
+                    Valor = double.Parse(Console.ReadLine().ToString(CultureInfo.CurrentCulture));
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"Pagamento realizado no valor de R$:{Valor} na função CREDITO");
+                    Thread.Sleep(4000);
+
+                    //SALVAR ARQUIVO DE PAGAMENTO
+                    salva.SalvarValor(Valor);
+                }
+                else
+                {
+                    Console.WriteLine("Opção incorreta! escreva uma opção valida!");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    PagarComCartao();
+                    Thread.Sleep(2000);
+                }
 
             }
             //VALIDAÇÃO ERRADA
@@ -105,8 +142,12 @@ namespace BankingPOO
                 Thread.Sleep(2000);
                 Console.Clear();
                 PagarComCartao();
+                Thread.Sleep(2000);
             }
 
+            //RETORNA AO MENU PRINCIPAL OU SAI
+
+            menu.RetornarAoMenu();
         }
     }
 
@@ -116,19 +157,50 @@ namespace BankingPOO
     public class PagamentoBoleto : DadosUsuario
     {
 
+
         public DateTime Vencimento { get; set; }
 
-        public Random NumeroBoleto { get; set; }
 
-        public double ValorBoleto { get; set; }
 
+        public double Valor { get; set; }
+
+        //PAGA O BOLETO
         public void PagarBoleto()
         {
-            NumeroBoleto = new Random(1000000000);
-            Console.WriteLine($"Codigo de pagamento  boleto: {NumeroBoleto}");
+            Menu menu = new Menu();
+            SalvarPagamentos salvar = new SalvarPagamentos();
+            //VALOR DO BOLETO
+            Console.WriteLine("Qual o valor de pagamento do boleto?");
+            Valor = double.Parse(Console.ReadLine());
+
+            //CODIGO BOLETO
+            string codigoBoleto = GerarCodigoBoleto();
+            Console.WriteLine($"Codigo de pagamento  boleto: {codigoBoleto}");
+            Console.WriteLine("Pressione ENTER quando realizar o pagamento!");
+            var ContinuarPagamentoBoleto = Console.ReadKey().Key != ConsoleKey.Enter;
+
+            Console.WriteLine("Pagando......");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Pagamento realizado com sucesso! no valor de R${Valor}");
+            Thread.Sleep(2000);
+            salvar.SalvarValor(Valor);
+            //VOLTA AO MENU
+            menu.RetornarAoMenu();
+
         }
 
-
+        //Gera o codigo do boleto
+        static string GerarCodigoBoleto()
+        {
+            Random random = new Random();
+            // Gera um número aleatório com 44 dígitos, que é o tamanho padrão de um código de boleto
+            string codigo = "";
+            for (int i = 0; i < 44; i++)
+            {
+                codigo += random.Next(0, 10).ToString();
+            }
+            return codigo;
+        }
 
     }
 
@@ -146,8 +218,9 @@ namespace BankingPOO
             Console.WriteLine("1 - Pagamento com cartão");
             Console.WriteLine("2 - Pagamento com boleto");
             Console.WriteLine("3 - Pagamento com pix");
-           
+
             Console.WriteLine("0 - Sair");
+
             short OpcaoSelecionada = short.Parse(Console.ReadLine());
             switch (OpcaoSelecionada)
             {
@@ -180,7 +253,9 @@ namespace BankingPOO
                     break;
 
 
+
             }
+
         }
         //METODO PARA RETORNAR AO MENU FACILMENTE
         public void RetornarAoMenu()
@@ -194,7 +269,8 @@ namespace BankingPOO
                 Thread.Sleep(2000);
                 Console.Clear();
                 Menuzada();
-            }else if (RetornoMenu == "sair")
+            }
+            else if (RetornoMenu == "sair")
             {
                 Console.WriteLine("Obrigado por usar meu app! saindo...");
                 Thread.Sleep(2000);
@@ -204,7 +280,7 @@ namespace BankingPOO
             {
                 Console.WriteLine("Opção invalida tente novamente! ");
                 Thread.Sleep(2000);
-                
+                Console.Clear();
                 RetornarAoMenu();
             }
 
@@ -212,5 +288,48 @@ namespace BankingPOO
 
         }
     }
+
+    //SALVAR ARQUIVO
+    public class SalvarPagamentos
+    {
+        Menu menu = new Menu();
+        public void SalvarValor(double Valor)
+        {
+            Console.WriteLine("Deseja salvar o arquivo ? SIM/NAO");
+            var resposta = Console.ReadLine().ToLower();
+            if (resposta == "sim" || resposta == "s")
+            {
+                Console.Clear();
+                Console.WriteLine("Qual caminho deseja salvar o arquivo? ");
+                var path = Console.ReadLine();
+                using (var arquivo = new StreamWriter(path))
+                {
+                    //salvando arquivo o texto inserido no path
+                    arquivo.WriteLine("Valor do pagamento: " + Valor);
+
+                }
+                Console.Clear();
+                Console.WriteLine($"Arquivo salvo com sucesso em {path}");
+                Console.WriteLine("Voltando ao menu principal");
+                Thread.Sleep(3500);
+                Console.Clear();
+                menu.Menuzada();
+            }
+            else if (resposta == "nao" || resposta == "n")
+            {
+                Console.WriteLine("Arquivo não salvo! ");
+                Console.WriteLine("Voltando ao menu principal");
+                Thread.Sleep(3500);
+                Console.Clear();
+                menu.Menuzada();
+            }
+            else
+            {
+                Console.WriteLine("Opção invalida! tente novamente");
+                SalvarValor(Valor);
+            }
+        }
+    }
+
 }
 
