b94d54e859951309a1013bf13f6bff84d79ca786
AldebaraFork <eduardoamorim758@proton.me>
2024-07-05 23:09:59 -0300

    Starting Card payment

--- PaymentSystem/Program.cs
+++ PaymentSystem/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PaymentSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public interface IPagamento
+        {
+            double Valor { get; set; }
+            DateTime Vencimento { get; set; }
+
+            Guid ReciboPagamento { get; set; }
+        }
+
+        public class PagamentoCartao : IPagamento
+        {
+            //INTERFACE 
+            public double Valor { get; set; }
+            public DateTime Vencimento { get; set; }
+            public Guid ReciboPagamento { get; set; }
+
+            //DADOS CARTÃO 
+            protected string NumeracaoCartao { get; set; }
+            protected string CVV { get; set; }
+            protected string NomeCartao { get; set; }
+
+            public void PagarCartao()
+            {
+                Console.WriteLine("")
+            }
+        }
+    }
+}
